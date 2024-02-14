3e92d2b45245ecccf8f1b9e1bb918f21f1825852
Kexin Zhu <coco99166@outlook.com>
2024-02-14 15:56:44 -0800

    add click to step icons

--- src/App.js
+++ src/App.js
@@ -27,9 +27,24 @@ export default function App() {
       {isOpen && (
         <div className="steps">
           <div className="numbers">
-            <div className={step >= 1 ? "active" : ""}>1</div>
-            <div className={step >= 2 ? "active" : ""}>2</div>
-            <div className={step >= 3 ? "active" : ""}>3</div>
+            <div
+              className={step >= 1 ? "active" : ""}
+              onClick={() => setStep(1)}
+            >
+              1
+            </div>
+            <div
+              className={step >= 2 ? "active" : ""}
+              onClick={() => setStep(2)}
+            >
+              2
+            </div>
+            <div
+              className={step >= 3 ? "active" : ""}
+              onClick={() => setStep(3)}
+            >
+              3
+            </div>
           </div>
 
           <p className="message">
