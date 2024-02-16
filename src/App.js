dcecad5b2d39d3c492a0f6dbd0554b5e11d8ed6e
Kexin Zhu <coco99166@outlook.com>
2024-02-15 18:01:44 -0800

    update state without current state

--- src/App.js
+++ src/App.js
@@ -11,16 +11,17 @@ export default function App() {
   const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    // if (step > 1) setStep(step - 1); don't update state using current state
+    if (step > 1) setStep((s) => s - 1);
   }
 
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
   }
 
   return (
     <>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+      <button className="close" onClick={() => setIsOpen((open) => !open)}>
         &times;
       </button>
 
